aafe700ac9b26cc8e0cb3ebe2e55a51c973bed09
Manas <122993008+manas-143@users.noreply.github.com>
2023-09-14 14:08:20 +0530

    Update restaurantdetails.py

--- features/steps/restaurantdetails.py
+++ features/steps/restaurantdetails.py
@@ -1,86 +1,102 @@
+from behave import *
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 import csv
 import time
-from behave import given, when, then
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 
-output_file_name = "google_map_restaurants_data.csv"
-unique_check = []
-number = 30
+"""locators for the webpage"""
+tags = {
+    "INPUT_AREA": "//*[@jslog='11886']",
+    "SEARCH_BTN": "//*[@jslog='11887']",
+    "SEARCH ITEMS": "//*[@class='Nv2PK THOPZb CpccDe ']/child::a",
+    "PLACE_NAME": "//h1[@class='DUwDvf lfPIob']",
+    "RATINGS": "//span[@class='ceNzKf']/preceding-sibling::span",
+    "REVIEW": "(//div[@class='F7nice ']/child::span)[2]",
+    "ADDRESS": "(//div[@class='rogA2c ']/child::div)[1]"
 
-def save_data(data):
-    header = ['id', 'company_name', 'rating', 'reviews_count', 'address']
-    with open(output_file_name, 'a', newline='', encoding="utf-8") as csvfile:
-        writer = csv.writer(csvfile)
-        if data[0] == 1:
-            writer.writerow(header)
-        writer.writerow(data)
+}
+"""............................."""
 
-def rating_and_review_count(restaurant):
-    try:
-        reviews_block = restaurant.find_element(By.CLASS_NAME, 'AJB7ye').text.split("(")
-        rating = reviews_block[0].strip()
-        reviews_count = reviews_block[1].split(")")[0].strip()
-    except:
-        rating = "no ratings"
-        reviews_count = "no reviews"
-
-    return rating, reviews_count
-
-def address_and_category(restaurant):
-    try:
-        address_block =restaurant.find_elements(By.CLASS_NAME, "W4Efsd")[2].text.split("·")
-        if len(address_block) >= 2:
-            address = address_block[1].strip()
-        elif len(address_block) == 1:
-            address = "address not available"
-    except:
-        address = "address not available"
-
-    return address
-
-@given("the user is on the Google Maps restaurants page")
-def step_impl_given(context):
-    url = "https://www.google.com/maps/search/restaurants+near+me/"
-    context.driver.get(url)
-    time.sleep(5)
-
-@when("the user scrolls down and loads more restaurants")
-def step_impl_when(context):
-    global unique_check, number
-
-    def get_restaurant_info():
-        global unique_check, number
-
-        time.sleep(2)
-        for business in context.driver.find_elements(By.CLASS_NAME, 'THOPZb'):
-            name = business.find_element(By.CLASS_NAME, 'fontHeadlineSmall').text
-            rating, reviews_count = rating_and_review_count(business)
-            address = address_and_category(business)
-
-            unique_id = "".join([name, rating, reviews_count, address])
-            if unique_id not in unique_check and len(unique_check) != number:
-                data = [name, rating, reviews_count, address]
-                save_data(data)
-                unique_check.append(unique_id)
-
-    panel_xpath = "//*[@class='m6QErb DxyBCb kA9KIf dS8AEf ecceSd']"
-    scrollable_div =context.driver.find_element(By.XPATH, panel_xpath)
+places_list = []  # to store the final details of the places
 
 
-    i = 0
-    while len(unique_check) != number:
-        print(f"Scrolling to page {i + 2}")
-        context.driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight', scrollable_div)
-        time.sleep(2)
-        get_restaurant_info()
-        i += 1
+@given(u'the user is on the Google Maps search for "{places}"')
+def search_for_places(context, places):
+    context.name = places
+    context.wait = WebDriverWait(context.driver, 10)
 
-@then("the user should scrape and save information for {number:d} restaurants")
-def step_impl_then(context, number):
-    assert len(unique_check) == number
+    context.driver.get("https://www.google.com/maps")  # going to google map webpage
+    search_box = context.wait.until(ec.presence_of_element_located((By.XPATH, tags["INPUT_AREA"])))
+    search_box.send_keys(places)
+    search_button = context.driver.find_element(By.XPATH, tags["SEARCH_BTN"])
+    search_button.click()  # searching for the places
 
-@then("the user should not have duplicate restaurant information")
-def step_impl_then(context):
-    unique_set = set(unique_check)
-    assert len(unique_set) == len(unique_check)
+
+@when(u'the user scrolls down and loads "{num}" more places')
+def scrolling_and_loading_the_place_details(context, num):
+    total = int(num)  # converting the string format num to int
+
+    """function for extracting restaurant details"""
+
+    def places_info(arg):
+        for i in range(total):
+
+            d = {}
+            arg[i].click()
+            context.driver.implicitly_wait(20)
+            time.sleep(5)  # used time.sleep as browser taking more time to load
+            try:
+                context.wait.until(ec.visibility_of_element_located((By.XPATH, tags["PLACE_NAME"])))
+                d["name"] = context.driver.find_element(By.XPATH, tags["PLACE_NAME"]).text
+            except:
+                d["name"] = "name not found"
+            try:
+                context.wait.until(ec.visibility_of_element_located((By.XPATH, tags["RATINGS"])))
+                d["ratings"] = context.driver.find_element(By.XPATH, tags["RATINGS"]).text
+            except:
+                d["ratings"] = "rating not found"
+            try:
+                context.wait.until(ec.visibility_of_element_located((By.XPATH, tags["REVIEW"])))
+                d["review"] = context.driver.find_element(By.XPATH, tags["REVIEW"]).text
+            except:
+                d["review"] = "review not found"
+            try:
+                context.wait.until(ec.visibility_of_element_located((By.XPATH, tags["ADDRESS"])))
+                d["address"] = context.driver.find_element(By.XPATH, tags["ADDRESS"]).text
+            except:
+                d["address"] = "address not found"
+
+            """filtering and adding the details into the list"""
+
+            if len(places_list) != total and d not in places_list:
+                places_list.append(d)
+                context.driver.find_element(By.XPATH, tags["SEARCH ITEMS"]).send_keys(Keys.DOWN)
+
+    """-----------------------------------------"""
+
+    places = []  # to store the list of the places displayed on the search result
+
+    """running an infinite loop for scrolling down until user gets desire output"""
+    while len(places) <= total:
+        places = context.wait.until(ec.presence_of_all_elements_located((By.XPATH, tags["SEARCH ITEMS"])))
+        single_item = context.wait.until(ec.presence_of_element_located((By.XPATH, tags["SEARCH ITEMS"])))
+        single_item.send_keys(Keys.END)  # scrolling to the end
+   
+    places_info(places)  # running the function to extract the details
+
+
+@then(u'the user should scrape and save information for the places in a csv file')
+def scrape_and_save_the_information_in_CSV(context):
+    # Specify the CSV file path
+    csv_file_path = f'{context.name}'
+
+    # Extract the keys from the first dictionary in the list (assuming all dictionaries have the same keys)
+    field_names = places_list[0].keys()
+
+    # Write data to CSV
+    with open(csv_file_path, 'w', newline='', encoding='utf-8') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=field_names)
+        writer.writeheader()
+        writer.writerows(places_list)
